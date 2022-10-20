be6ed2dced53840759afac769a3a916e4e00348d
MrunmayeeManmadkar <mrunmayee.manmadkar@cumminscollege.in>
2022-10-20 14:56:44 +0530

    Commit Android app SKMDecor

---
+++ app/src/main/java/com/example/skmdecor/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.skmdecor.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Skyblue=Color(0x5cacbd)
+val Brown=Color(0x964B00)
+val greenColor = Color(0xFF0F9D58)
